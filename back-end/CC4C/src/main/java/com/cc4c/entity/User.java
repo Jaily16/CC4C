50acb156faaa8408539df225c1f3677a22b5010d
flyingdoraemon <90094320+flyingdoraemon@users.noreply.github.com>
2022-12-12 21:18:34 +0800

    Add files via upload

--- back-end/CC4C/src/main/java/com/cc4c/entity/User.java
+++ back-end/CC4C/src/main/java/com/cc4c/entity/User.java
@@ -1,19 +1,27 @@
 package com.cc4c.entity;
 
+import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
 import lombok.Data;
 
+import java.util.Date;
+
 @Data
+@TableName(value = "user")
 public class User {
-    @TableId
-    private Long userId;
-    private String userName;
-    private String email;
-    private String password;
-    private Integer major;
-    private String avatar;
-    private Integer state;
-    private String createTime;
-    private Integer favouriteLanguage;
+  @TableId(value = "user_id", type = IdType.ASSIGN_ID)
+  private Long id;
+  @TableField(value = "user_name")
+  private String name;
+  private String email;
+  private String password;
+  private Integer major;
+  private String avatar;
+  private Integer state;
+  @TableField(value = "create_time")
+  private Date time;
+  @TableField(value = "favourite_language")
+  private Integer language;
 }
